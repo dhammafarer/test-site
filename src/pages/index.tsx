514b391adb2ccedb0da3490c4f982fd7c128269e
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-03-25 14:10:06 +0800

    add image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
 import * as React from "react";
+import { graphql } from "gatsby";
+import GatsbyImage from "gatsby-image";
 
 export interface Props {
-  data: {};
+  data: {
+    img: any;
+  };
 }
 
-const IndexPage: React.SFC<Props> = () => {
-  return <div>text</div>;
+const IndexPage: React.SFC<Props> = props => {
+  console.log(props.data);
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <GatsbyImage fluid={props.data.img.childImageSharp.fluid} />
+    </div>
+  );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query IndexPageQuery {
+    img: file(relativePath: { eq: "solar-panels.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
