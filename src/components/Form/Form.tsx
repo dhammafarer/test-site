5e1433877d42794ae24f07587b3fee529a41a05d
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-04-02 18:51:28 +0800

    fix types in useFormValidation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,104 +2,15 @@ import * as React from "react";
 import { Button, Box, Card } from "primithemes";
 import { Input } from "./Input";
 import { isRequired, isAdult } from "./validations";
-
-type Validator = (value: string) => string;
-
-type Values = { [key: string]: string };
-type Errors = { [key: string]: React.ReactNode[] };
-
-interface Fields {
-  [key: string]: {
-    value: string;
-    validators: Validator[];
-  };
-}
-
-interface Messages {
-  [key: string]: React.ReactNode;
-}
-
-const useFormValidation = (fields: Fields, messages: Messages) => {
-  const [values, setValues] = React.useState(
-    Object.keys(fields).reduce(
-      (a, b) => ({ ...a, [b]: fields[b].value }),
-      {} as Values
-    )
-  );
-  const initialErrors = Object.keys(fields).reduce(
-    (a, b) => ({ ...a, [b]: [] }),
-    {} as Errors
-  );
-
-  const [errors, setErrors] = React.useState(initialErrors);
-
-  const updateValue = (e: React.FormEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    const name = e.currentTarget.name;
-    const newState = Object.assign({}, values, {
-      [name]: value,
-    });
-    setValues(newState);
-  };
-
-  const updateErrorsFor = (name: string, errs: React.ReactNode[]) => {
-    setErrors({ ...errors, [name]: errs });
-  };
-
-  const validate = (value: string, name: string) => {
-    const valFns = fields[name].validators as Validator[];
-    if (!valFns) return [];
-    const errs = valFns.reduce(
-      (a, b) => {
-        const err = b(value);
-        if (err) a.push(messages[err]);
-        return a;
-      },
-      [] as React.ReactNode[]
-    );
-    return errs;
-  };
-
-  const handleBlur = (e: React.FormEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    const name = e.currentTarget.name;
-    updateErrorsFor(name, validate(value, name));
-  };
-
-  const validateForm = () => {
-    return Object.keys(fields).reduce(
-      (a, b) => {
-        const errs = validate(values[b], b);
-        if (errs.length < 1) return [true, a[1]];
-        return [false, Object.assign({}, a[1], { [b]: errs })];
-      },
-      [false, {}]
-    );
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    const [valid, errs] = validateForm();
-    if (!valid) {
-      e.preventDefault();
-      console.log(errs);
-      setErrors(errs);
-    }
-  };
-
-  return {
-    values,
-    setValues,
-    errors,
-    setErrors,
-    handleBlur,
-    handleSubmit,
-    updateValue,
-  };
-};
+import { useFormValidation } from "./useFormValidation";
 
 const Form: React.SFC<{}> = props => {
   const fields = {
     name: {
+      value: "",
+      validators: [isRequired],
+    },
+    address: {
       value: "",
       validators: [isRequired],
     },
@@ -133,6 +44,18 @@ const Form: React.SFC<{}> = props => {
               required
               errors={errors.name}
               value={values.name}
+              handleChange={updateValue}
+              handleBlur={handleBlur}
+            />
+          </Box>
+          <Box>
+            <Input
+              name="address"
+              type="text"
+              title="Address"
+              required
+              errors={errors.address}
+              value={values.address}
               handleChange={updateValue}
               handleBlur={handleBlur}
             />
---
+++ src/components/Form/useFormValidation.ts
@@ -0,0 +1,93 @@
+import * as React from "react";
+type Validator = (value: string) => string;
+
+type Fields<T> = {
+  [P in keyof T]: {
+    value: string;
+    validators: Validator[];
+  }
+};
+
+type Values<T> = { [P in keyof T]: string };
+type Errors<T> = { [P in keyof T]: React.ReactNode[] };
+
+interface Messages {
+  [key: string]: React.ReactNode;
+}
+
+export const useFormValidation = <T>(fields: Fields<T>, messages: Messages) => {
+  const [values, setValues] = React.useState(
+    Object.keys(fields).reduce(
+      (a, b) => ({ ...a, [b]: fields[b].value }),
+      {} as Values<T>
+    )
+  );
+  const initialErrors = Object.keys(fields).reduce(
+    (a, b) => ({ ...a, [b]: [] }),
+    {} as Errors<T>
+  );
+
+  const [errors, setErrors] = React.useState(initialErrors);
+
+  const updateValue = (e: React.FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    const name = e.currentTarget.name;
+    const newState = Object.assign({}, values, {
+      [name]: value,
+    });
+    setValues(newState);
+  };
+
+  const updateErrorsFor = (name: string, errs: React.ReactNode[]) => {
+    setErrors({ ...errors, [name]: errs });
+  };
+
+  const validate = (value: string, name: string) => {
+    const valFns = fields[name].validators as Validator[];
+    if (!valFns) return [];
+    const errs = valFns.reduce(
+      (a, b) => {
+        const err = b(value);
+        if (err) a.push(messages[err]);
+        return a;
+      },
+      [] as React.ReactNode[]
+    );
+    return errs;
+  };
+
+  const handleBlur = (e: React.FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    const name = e.currentTarget.name;
+    updateErrorsFor(name, validate(value, name));
+  };
+
+  const validateForm = () => {
+    return Object.keys(fields).reduce(
+      (a, b) => {
+        const errs = validate(values[b], b);
+        if (errs.length < 1) return { valid: true, errs: a.errs };
+        return { valid: false, errs: Object.assign({}, a.errs, { [b]: errs }) };
+      },
+      { valid: false, errs: {} } as { valid: boolean; errs: Errors<T> }
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const { valid, errs } = validateForm();
+    if (!valid) {
+      e.preventDefault();
+      setErrors(errs);
+    }
+  };
+
+  return {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleBlur,
+    handleSubmit,
+    updateValue,
+  };
+};
