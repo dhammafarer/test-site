0ed6f41f262b0fdfb04daa7edb4605d9d50651a1
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-04-02 15:32:54 +0800

    add validateAll

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Box, Card } from "primithemes";
+import { Button, Box, Card } from "primithemes";
 import { Input } from "./Input";
 import { isRequired } from "./validations";
 
 type Validator = (value: string) => string[];
 
 const Form: React.SFC<{}> = props => {
-  const [personal, setPersonal] = React.useState({
+  const [values, setValues] = React.useState({
     name: "",
     birthdate: "",
   });
@@ -17,10 +17,10 @@ const Form: React.SFC<{}> = props => {
   const updateValue = (e: React.FormEvent<HTMLInputElement>) => {
     const value = e.currentTarget.value;
     const name = e.currentTarget.name;
-    const newState = Object.assign({}, personal, {
+    const newState = Object.assign({}, values, {
       [name]: value,
     });
-    setPersonal(newState);
+    setValues(newState);
   };
 
   const validate = (e: React.FormEvent<HTMLInputElement>) => {
@@ -32,36 +32,59 @@ const Form: React.SFC<{}> = props => {
       .map((x: Validator) => x(value))
       .filter((x: string) => x !== "");
     setErrors(Object.assign({}, { ...errors, [name]: errs }));
-    console.log(errors);
+  };
+
+  const validateAll = () => {
+    const errs = Object.keys(validators).reduce(
+      (a, b) => {
+        a[b] = validators[b].map(x => x(values[b])).filter(x => x !== "");
+        return a;
+      },
+      { name: [], birthdate: [] }
+    );
+    setErrors(errs);
+    return Object.keys(errs).find(k => errs[k].length > 0) ? false : true;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (validateAll()) e.currentTarget.submit();
   };
 
   return (
     <Box p={3}>
       <Card shadow={1} radius={2} p={3} bg="grey.100">
-        <Box>
-          <Input
-            name="name"
-            type="text"
-            title="Name"
-            required
-            errors={errors.name}
-            value={personal.name}
-            handleChange={updateValue}
-            handleBlur={validate}
-          />
-        </Box>
-        <Box>
-          <Input
-            name="birthdate"
-            type="date"
-            required
-            title="Date"
-            value={personal.birthdate}
-            handleChange={updateValue}
-            errors={errors.birthdate}
-            handleBlur={validate}
-          />
-        </Box>
+        <form onSubmit={handleSubmit}>
+          <Box>
+            <Input
+              name="name"
+              type="text"
+              title="Name"
+              required
+              errors={errors.name}
+              value={values.name}
+              handleChange={updateValue}
+              handleBlur={validate}
+            />
+          </Box>
+          <Box>
+            <Input
+              name="birthdate"
+              type="date"
+              required
+              title="Date"
+              value={values.birthdate}
+              handleChange={updateValue}
+              errors={errors.birthdate}
+              handleBlur={validate}
+            />
+          </Box>
+          <Box mt={3}>
+            <Button radius={2} p={2} type="submit">
+              Submit
+            </Button>
+          </Box>
+        </form>
       </Card>
     </Box>
   );
