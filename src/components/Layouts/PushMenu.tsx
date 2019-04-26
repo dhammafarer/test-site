f96050e80fd732f901e2333d12fb74122603e4a4
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-04-26 15:50:30 +0800

    use flex

--- src/components/Layouts/PushMenu.tsx
+++ src/components/Layouts/PushMenu.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import styled, { css } from "styled-components";
 import { space, color, shadow } from "src/theme";
 import { animated, useSpring } from "react-spring";
-import { tablet } from "src/theme/media";
 import { useMedia } from "src/hooks/useMedia";
 
 const width = "250px";
 
 const Root = styled.div`
   position: relative;
+  display: flex;
   width: 100%;
   overflow: hidden;
   height: 100vh;
@@ -17,38 +17,31 @@ const Root = styled.div`
 
 const Main = styled(animated.div)`
   z-index: 1;
+  flex-grow: 1;
   height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transform: translate3d(0px, 0, 0);
-  right: 0;
-  bottom: 0;
   background: ${color("grey.100")};
   transition: left 400ms ease-out;
   box-shadow: ${shadow(1)};
   overflow-x: hidden;
-  overflow-x: auto;
-  ${tablet(css`
-    left: 10px;
-  `)}
+  overflow-y: scroll;
+  min-width: 400px;
 `;
 const Sidebar = styled(animated.div)`
   z-index: 0;
-  position: absolute;
-  top: 0;
-  left: 0px;
-  bottom: 0;
+  flex: 0 0 auto;
   overflow: auto;
   width: ${width};
   background: ${color("grey.400")};
   height: 100vh;
-  transition: left 400ms ease-out;
 `;
 
 const TopBar = styled.div`
   width: 100%;
   padding: ${space(2)};
+  position: sticky;
+  top: 0;
+  left: 0;
+  background: ${color("divider.light")};
 `;
 const Container = styled.div`
   max-width: 960px;
@@ -66,14 +59,16 @@ const PushMenu: React.SFC<{}> = ({ children }) => {
   const val = useMedia(["(min-width: 600px)"], [true], false);
   const [open, toggle] = React.useState(() => (val ? true : false));
   const props = useSpring({
-    left: val && open ? width : val ? "50px" : "0px",
-    transform:
-      !val && open ? `translate3d(${width},0,0)` : "translate3d(0px,0,0)",
+    width: open ? width : "0px",
+    background: open ? color("grey.200") : color("grey.400"),
+    overflow: open ? "auto" : "hidden",
   });
-  React.useEffect(() => void console.log(val));
   return (
     <Root>
-      <Main style={props}>
+      <Sidebar style={props}>
+        <div style={{ height: "140vh" }}>content</div>
+      </Sidebar>
+      <Main>
         <TopBar>
           <button onClick={() => toggle(!open)}>toggle</button>
         </TopBar>
@@ -85,7 +80,6 @@ const PushMenu: React.SFC<{}> = ({ children }) => {
           ))}
         </Container>
       </Main>
-      <Sidebar>sidebar</Sidebar>
     </Root>
   );
 };
